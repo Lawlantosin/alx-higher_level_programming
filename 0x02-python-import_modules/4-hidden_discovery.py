062c0eb828d38a8c2d59f699dac310db4080e6eb
Adeoye oluwatosin <lawlantosin@gmail.com>
2022-12-01 07:38:33 -0800

    commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+    import sys
+    import hidden_4
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
