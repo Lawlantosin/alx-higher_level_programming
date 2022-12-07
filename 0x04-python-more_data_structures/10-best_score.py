88ea3fb291daa928fd9f3412f7bd143938da5b8c
Adeoye oluwatosin <lawlantosin@gmail.com>
2022-12-07 14:52:06 -0800

    second commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    if not a_dictionary:
+        return None
+    biggest = 0
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = value
+    for key, value in a_dictionary.items():
+        if value == biggest:
+            return key
