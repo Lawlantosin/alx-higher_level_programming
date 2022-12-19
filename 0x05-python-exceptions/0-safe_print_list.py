e34a10678b73de67abba36bfe457c0aa7bc1ee0f
Adeoye oluwatosin <lawlantosin@gmail.com>
2022-12-19 13:34:23 -0800

    ínitial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    a = 0
-    for b in range(x):
-        try:
-            print("{}".format(my_list[b]), end="")
-            a += 1
-        except IndexError:
-            break
-    print("")
-    return(a)
+    try:
+        count = 0
+        while count < x:
+            print("{}".format(my_list[count]), end='')
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
+        pass
