c91baa794287f2e2678c9c188cde6edbab487cf7
Adeoye oluwatosin <lawlantosin@gmail.com>
2023-01-09 14:47:16 -0800

    best commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains a class
+with public instance and Raises
+exception when required
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle with private height and width
+    """
+    def __init__(self, width, height):
+        """
+        instantiation of class
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height 
