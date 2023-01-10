9fe100c64d9fb040f404b4db36d3da223207ac61
Adeoye oluwatosin <lawlantosin@gmail.com>
2023-01-10 14:35:16 -0800

    initial commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""
-This file contains function that appends
-a string at the end of a text file and
-returns number of characters added
-"""
-
-
 def append_write(filename="", text=""):
-    with open(filename, mode="a", encoding="utf-8") as myFile:
-        return (myFile.write(str(text)))
+    with open(filename, mode="a", encoding="utf-8") as fd:
+        fd.write(text)
+    return len(text)
